e1d148c759d82433cce2ba8fea444031dd915ec0
盧韋宏 <whl@wke.csie.ncnu.edu.tw>
2021-07-02 22:37:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
 		return (
 			<div className="App">
 				<div className="App-header">
-					<img src={logo} className="App-logo" alt="React logo" />
+					{/* <img src={logo} className="App-logo" alt="React logo" /> */}
 					<h2>Welcome to React</h2>
 				</div>
 				<p className="App-intro">
